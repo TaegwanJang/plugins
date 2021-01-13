3ad6a576b37caa35a2071b591a3d2ad7b47902a2
정동휘 <dowu1012@gmail.com>
2021-01-13 14:09:42 +0900

    <input type='file'/> WebChromeClient -> FlutterWebChromeClient

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
@@ -105,12 +105,11 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
 
         // Multi windows is set with FlutterWebChromeClient by default to handle internal bug: b/159892679.
         webView.getSettings().setSupportMultipleWindows(true);
-        // webView.setWebChromeClient(new FlutterWebChromeClient());
         /**
          * start
          * */
         context1 = context;
-        webView.setWebChromeClient(new WebChromeClient() {
+        webView.setWebChromeClient(new FlutterWebChromeClient(){
             @Override
             public boolean onShowFileChooser(
                     WebView webView, ValueCallback<Uri[]> filePathCallback,
@@ -130,8 +129,26 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
             }
 
         });
-
-
+//        webView.setWebChromeClient(new WebChromeClient() {
+//            @Override
+//            public boolean onShowFileChooser(
+//                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+//                    FileChooserParams fileChooserParams) {
+//
+//                //成功跳转newActivity！！！很 nice
+//                //跳转到newActivity去打开文件夹的操作
+//                Intent intent = new Intent(context1, newActivity.class);
+//                newActivity.getfilePathCallback(filePathCallback);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                context1.startActivity(intent);
+//                return true;
+//            }
+//
+//            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+//                callback.invoke(origin, true, false);
+//            }
+//
+//        });
 /**
  * end
  * */
